af3fac0be14d4a6011bbd6964d57abfb888fbd41
John Schlachtenhaufen <jschlach25@gmail.com>
2019-04-11 16:37:48 -0400

    added webpack, babel, eslint, and sass

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("HI")
+import $ from 'jquery';
+import './style.scss';
+
+$('#main').html('Here we go!');
+
+let time = 1;
+setInterval(() => {
+    $('#main').html(`You've been here for ${time} seconds`);
+    time += 1;
+}, 1000);
