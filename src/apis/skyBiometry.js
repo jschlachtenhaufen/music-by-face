ff3ab43669a6b8be0999cca7e4955e7485bbb00f
John Schlachtenhaufen <jschlach25@gmail.com>
2019-08-19 19:00:34 -0500

    switch to domain auth for sky bio

--- src/apis/skyBiometry.js
+++ src/apis/skyBiometry.js
@@ -1,18 +1,15 @@
 import axios from 'axios';
 import FormData from 'form-data';
 
-const API_KEY = '';
-const API_SECRET = '';
 const API_URL = 'http://api.skybiometry.com/fc/faces/detect.json';
+const SKY_API_KEY = '772mtgive8q17940mhiqo6og51'; // uses domain authentication, so exposure is okay
 
 const skyBiometry = {
-    detectFaces: (files) => {
+    detectFaces: (file) => {
         return new Promise((resolve, reject) => {
             const data = new FormData();
-            files.forEach((file) => {
-                data.append('image', file);
-            });
-            axios.post(`${API_URL}?api_key=${API_KEY}&api_secret=${API_SECRET}&attributes=all`, data, {
+            data.append('image', file);
+            axios.post(`${API_URL}?api_key=${SKY_API_KEY}&attributes=all`, data, {
                 headers: {
                     accept: 'application/json',
                     'Accept-Language': 'en-US,en;q=0.8',
@@ -20,9 +17,10 @@ const skyBiometry = {
                 },
             }).then((response) => {
                 resolve(response.data.photos);
-                // TODO process before resolving?
             }).catch((error) => {
-                reject(error);
+                const message = error.response.data.error_message;
+                reject(message);
+                // TODO other handling?
             });
         });
     },
